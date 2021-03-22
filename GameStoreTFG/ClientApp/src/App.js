1041b036cbe39a9ba30fa4d31ddfc5623f852a83
Fernando Jiménez Iglesias <0619246531@uma.es>
2021-03-22 16:51:29 +0100

    Clean UI

--- GameStoreTFG/ClientApp/src/App.js
+++ GameStoreTFG/ClientApp/src/App.js
@@ -1,20 +1,12 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React  from 'react';
+import './App.css';
 
-export default class App extends Component {
-  displayName = App.name
-
-  render() {
+function App() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
+        <div className="App">
+            Hello World!!
+        </div>
     );
-  }
 }
+
+export default App;
